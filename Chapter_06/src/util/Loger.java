537776adbb3c5f70fafa475c33eb2c20a9638229
icer-CHN <icer.CHN@outlook.com>
2015-08-26 20:04:55 +0800

    init Chapter 06

---
+++ Chapter_06/src/util/Loger.java
@@ -0,0 +1,21 @@
+package util;
+
+/**
+ * For log.
+ * Created by icer on 2015/8/22.
+ */
+public class Loger {
+
+    public static void logLn() {
+        logLn("");
+    }
+
+    public static void logLn(String msg) {
+        System.out.println(msg);
+    }
+
+    public static void log(String msg) {
+        System.out.print(msg);
+    }
+
+}
